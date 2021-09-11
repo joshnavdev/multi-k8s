03ebebe2536a64adcc63c659a12f831510d0fb5e
Joshua Navarro <joshua.navarro35@gmail.com>
2021-09-11 14:01:37 -0500

    added deployment scripts

---
+++ deploy.sh
@@ -0,0 +1,16 @@
+docker build -t joshnavru/multi-client:latest -t joshnavru/multi-client:$SHA -f ./client/Dockerfile ./client
+docker build -t joshnavru/multi-server:latest -t joshnavru/multi-server:$SHA -f ./server/Dockerfile ./server
+docker build -t joshnavru/multi-worker:latest -t joshnavru/mulri-worker:$SHA -f ./worker/Dockerfile ./worker
+
+docker push joshnavru/multi-client:latest
+docker push joshnavru/multi-server:latest
+docker push joshnavru/multi-worker:latest
+
+docker push joshnavru/multi-client:$SHA
+docker push joshnavru/multi-server:$SHA
+docker push joshnavru/multi-worker:$SHA
+
+kubectl apply -f k8s
+kubectl set image deployments/client-deployment client=joshnavru/multi-client:$SHA
+kubectl set image deployments/server-deployment server=joshnavru/multi-server:$SHA
+kubectl set image deployments/worker-deployment worker=joshnavru/multi-worker:$SHA
